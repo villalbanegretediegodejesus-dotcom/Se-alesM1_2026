75af64d1b05fa5f7e797cb44baf674f09813f11e
villalbanegretediegodejesus-dotcom <villalbanegretediegodejesus@gmail.com>
2026-09-22 17:00:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -259,8 +259,8 @@ async def analisis_automatico(context: ContextTypes.DEFAULT_TYPE):
         # ----------------------------------------------------
         # ENVIAR SENAL AUTOMATICA
         # ----------------------------------------------------
-global OPERACION_ACTIVA
-OPERACION_ACTIVA = True
+        global OPERACION_ACTIVA
+        OPERACION_ACTIVA = True
         await context.bot.send_message(
             chat_id=CHAT_ID,
             text=(
