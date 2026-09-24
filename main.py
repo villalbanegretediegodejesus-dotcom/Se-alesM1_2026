866c2e0b0d784ca882b32e04e90b543811427499
villalbanegretediegodejesus-dotcom <villalbanegretediegodejesus@gmail.com>
2026-09-24 18:53:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -543,9 +543,9 @@ def crear_bot():
     # ANALISIS AUTOMATICO CADA 5 MINUTOS
     #====================================================
     telegram_app.job_queue.run_repeating(
-    analisis_automatico,
-    interval=300,
-    first=10
+           analisis_automatico,
+           interval=300,
+           first=10
     )
     return telegram_app
 
